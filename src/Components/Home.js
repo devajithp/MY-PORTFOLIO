30282b3ad5185afe3fe50c2e0d30667f8f3566e7
Devajith <devajithmr@gmail.com>
2023-03-26 20:03:51 +0530

    landing page changed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,11 +5,29 @@ function Home() {
   return (
     <div style={{paddingTop:"100px"}} >
     
-     <img style={{width:"100%"}} src='https://res.cloudinary.com/dw7fovacw/image/upload/v1679409814/Untitled_rg5e0l.jpg'></img>
+    <img alt='cover pic' style={{width:"100%"}} src='https://res.cloudinary.com/dw7fovacw/image/upload/v1679840728/new_cover_s09ogx.jpg'></img>
+    
+    
 
-     {/* <iframe width="420" height="315"
-         src="https://www.youtube.com/embed/k3oDxlQtDH4">
-     </iframe> */}
+     <div className='container'>
+        <div style={{height:"400px",textAlign:"center",paddingTop:"10px"}} className='row'>
+          <h4 className='logo'>About</h4><hr></hr>
+          <div className='container'>
+            <div className='row'>
+          <div className='logo' style={{width:"800px", marginLeft:"auto",marginRight:"auto"}} >
+            <p style={{textAlign:"justify",height:"350px",overflowY:"scroll"}}>
+            As a MERN stack developer, I have experience using MongoDB, Express.js, React.js, and Node.js to create full-stack web applications. My portfolio includes projects that showcase my proficiency in each of these technologies, such as a full-stack web application that uses MongoDB for data storage, Express.js for server-side logic, React.js for dynamic user interfaces, and Node.js for server power.
+
+<br></br>I also have experience with other commonly used tools and technologies in MERN stack development, including Git, NPM, Webpack, Babel, and Redux. In addition, I have contributed to open source projects related to MERN stack development and have worked collaboratively on team projects.
+
+<br></br>My portfolio demonstrates my passion for creating high-quality web applications and showcases my technical skills as a MERN stack developer. I am confident in my ability to work collaboratively and to create dynamic and engaging web applications using the MERN stack. 
+<br></br> In addition to my technical skills and experience, I am committed to staying up-to-date with the latest trends and developments in MERN stack development. I continuously seek out new resources and participate in online communities to learn from other developers and stay current with emerging best practices and tools. I am passionate about creating user-friendly and visually appealing web applications that provide a seamless user experience. With my strong technical skills, collaborative mindset, and dedication to ongoing learning and improvement, I am excited to bring my expertise as a MERN stack developer to any project or team.
+            </p>
+          </div>
+          </div>
+          </div>
+        </div>
+     </div>
      
     </div>
   )
