8e5ee4c8e58686b8d4199a21f7223a06c88b7032
Devajith <devajithmr@gmail.com>
2023-03-26 22:40:00 +0530

    about modified

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -18,7 +18,7 @@ function Home() {
             <p style={{textAlign:"justify",height:"350px",overflowY:"scroll"}}>
             As a MERN stack developer, I have experience using MongoDB, Express.js, React.js, and Node.js to create full-stack web applications. My portfolio includes projects that showcase my proficiency in each of these technologies, such as a full-stack web application that uses MongoDB for data storage, Express.js for server-side logic, React.js for dynamic user interfaces, and Node.js for server power.
 
-<br></br>I also have experience with other commonly used tools and technologies in MERN stack development, including Git, NPM, Webpack, Babel, and Redux. In addition, I have contributed to open source projects related to MERN stack development and have worked collaboratively on team projects.
+<br></br>I also have experience with other commonly used tools and technologies in MERN stack development, including Git, NPM, Webpack, Babel, and Redux.
 
 <br></br>My portfolio demonstrates my passion for creating high-quality web applications and showcases my technical skills as a MERN stack developer. I am confident in my ability to work collaboratively and to create dynamic and engaging web applications using the MERN stack. 
 <br></br> In addition to my technical skills and experience, I am committed to staying up-to-date with the latest trends and developments in MERN stack development. I continuously seek out new resources and participate in online communities to learn from other developers and stay current with emerging best practices and tools. I am passionate about creating user-friendly and visually appealing web applications that provide a seamless user experience.
